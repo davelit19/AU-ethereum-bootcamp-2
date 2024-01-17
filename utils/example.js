c78a46475b78f76ceb97c1ea19c4ccb3c4dda0f9
davelit19 <omoledavid24714@gmail.com>
2024-01-17 03:29:51 +0100

    Alchemy university ethereum week 2 project

--- utils/example.js
+++ utils/example.js
@@ -15,6 +15,6 @@ const proof = merkleTree.getProof(index);
 
 // verify proof against the Merkle Root
  console.log( verifyProof(proof, name, root) ); // true, Norman Block is in the list!
-console.log(root)
+console.log()
 
 // TRY IT OUT: what happens if you try a name not in the list, or a fake proof?
